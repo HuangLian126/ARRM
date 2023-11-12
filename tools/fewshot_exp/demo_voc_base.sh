4f72cfda12627ed43af827cd50b9ce5af800dab9
HuangLian126 <51012563+HuangLian126@users.noreply.github.com>
2023-11-12 22:24:56 +0800

    Delete tools directory

--- tools/fewshot_exp/demo_voc_base.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export CUDA_VISIBLE_DEVICES=0
-export NGPUS=1
-SPLIT=(0.0)
-for split in ${SPLIT[*]} 
-do
-  configfile=configs/fewshot/base/e2e_voc_split1_base_${split}.yaml
-  python -m torch.distributed.launch --nproc_per_node=$NGPUS ./tools/demo.py --config-file ${configfile}
-done
